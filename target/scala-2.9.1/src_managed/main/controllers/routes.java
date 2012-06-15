3084ded21b013432e3c8a74c57b443e29c080db0
= <toshipon123@gmail.com>
2012-06-15 22:55:30 +0900

    Play frameworkプロジェクト作成

---
+++ target/scala-2.9.1/src_managed/main/controllers/routes.java
@@ -0,0 +1,19 @@
+// @SOURCE:/Users/toshipon/Public/dev/workspace/scala/anispot/conf/routes
+// @HASH:f904f23acbfbb0ded1d9ae85b262187e14e562de
+// @DATE:Fri Jun 15 22:39:33 JST 2012
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static class javascript {
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();    
+}   
+public static class ref {
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();    
+} 
+}
+                            
