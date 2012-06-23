0a291c9be16d3a85186caef3b3b7d28fd1387b02
= <toshipon123@gmail.com>
2012-06-23 16:00:24 +0900

    spディレクトリを作って移動

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,13 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
+import play.api._
+import com.sun.net.httpserver.HttpContext
 
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.sp.index("Your new application is ready."))
   }
   
 }
